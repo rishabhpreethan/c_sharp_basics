f11fbf33fc929b44c8bad436ed76f5bdb57c85d5
rishabhpreethan <rishabh.preethan@gmail.com>
2023-02-04 12:39:14 +0530

    committed

---
+++ c_sharp_basics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Internal;
+
+namespace c_sharp_basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            //PRINTING
+            Console.WriteLine("    /|");
+            Console.WriteLine("   / |");
+            Console.WriteLine("  /  |");
+            Console.WriteLine(" /___|");
+            //this keeps the output screen
+            //Console.ReadLine();
+
+
+
+            //variables
+            string name = "Rishabh";
+
+            int age = 20;
+            //or
+            //int age;
+            //age = 20;
+
+            char grade = 'A';
+
+
+
+            //printing variables
+            Console.WriteLine("His name is " + name+"and his age is "+age);
+            //this keeps the output screen
+            Console.ReadLine();
+        }
+    }
+}
